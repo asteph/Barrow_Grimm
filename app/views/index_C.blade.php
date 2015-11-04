dd018794f5a979928316aaa1f9f78f760ced8e37
asteph <alissa.stephens1@gmail.com>
2015-11-04 11:25:08 -0600

    removed Laravel framework for simplicity, worked on header and Areas of practice primarily

--- app/views/index_C.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-    <h1>Content Goes Here</h1>
-@stop
--- public/index.php
+++ public/index.php
@@ -1,49 +1,223 @@
-<?php
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
-|
-*/
+<!-- Basic Page Needs
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta charset="utf-8">
+	<title>Barrow &amp; Grimm - Home</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-require __DIR__.'/../bootstrap/autoload.php';
+<!-- Mobile Specific Metas
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let's turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight these users.
-|
-*/
+<!-- FONT
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,400italic' rel='stylesheet' type='text/css'>
 
-$app = require_once __DIR__.'/../bootstrap/start.php';
+<!-- CSS
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="/css/normalize.css">
+	<link rel="stylesheet" href="/css/skeleton.css">
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can simply call the run method,
-| which will execute the request and send the response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have whipped up for them.
-|
-*/
+<!-- Favicon
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="icon" type="image/png" href="/images/favicon.png">
 
-$app->run();
+<!-- Head information from each blade
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="/css/index_C.css">
+
+	<!-- section outlines for developement -->
+	<style>
+		.column, .columns{
+			border-style: solid;
+		}
+	</style>
+
+</head>
+<body>
+
+<!-- Primary Page Layout
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<div class="nav">
+	    <div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<h1>Logo and Navigation</h1>  
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- green embelishment -->
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns green"></div> 
+		</div>
+	</div>
+	<div class="hero section">
+	    <div class="container">
+			<div class="row">
+				<div class="twelve columns hero-text">
+					<span class="hero-heading">Counsel</span>
+					<span class="hero-heading-small">FOR THE</span><br>
+					<span class="hero-heading">Business of Life</span><br>
+					<div>Find out how we can help you with your legal needs</div>
+					<div class="button">Learn How</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- green embelishment -->
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns green"></div> 
+		</div>
+	</div>
+	<div class="split">
+		<div class="container">
+			<div class="row">
+				<div class="one-half column practice">
+					<h2><span class="practice-title">Areas of Practice</span></h2>
+					<a href="">Other Practice Areas </a>
+					<hr class="solid">
+					<div id="accordion">
+						<h3>Business Litigation</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+								<div class="button">More Info</div>
+							</p>
+						</div>
+						<h3>Estate Planning</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+							</p>
+						</div>
+						<h3>Labor &amp; Employment</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+							</p>
+						</div>
+						<h3>Tax Law</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+							</p>
+						</div>
+						<h3>Construction Litigation / Surety Law</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+							</p>
+						</div>
+						<h3>Alternatetive Dispute Resolution</h3>
+						<div>
+							<p>
+								Litigation is an integral part of Barrow &amp; Grimm’s practice. The firm’s lawyers have earned excellent reputations and the respect of their peers by offering extensive experience and skill from the smallest to the most complex litigation issues..
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="one-half column news">
+					<!-- NEWS ARTICLES GO HERE -->
+					<h2><span class="news-title">News</span></h2>
+					<a href="">View More News</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+
+	</footer>
+<!-- End Document 
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<!-- jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script>
+	$(function() {
+		$( "#accordion" ).accordion({
+			event: "click hoverintent",
+			// allows accordian boxes to be different heights that fit content
+			heightStyle: "content",
+			collapsible: true,
+			// starts with accordian closed
+			active: false
+		});
+	});
+
+	/*
+	* hoverIntent | Copyright 2011 Brian Cherne
+	* http://cherne.net/brian/resources/jquery.hoverIntent.html
+	* modified by the jQuery UI team
+	*/
+	$.event.special.hoverintent = {
+		setup: function() {
+			$( this ).bind( "mouseover", jQuery.event.special.hoverintent.handler );
+		},
+		teardown: function() {
+			$( this ).unbind( "mouseover", jQuery.event.special.hoverintent.handler );
+		},
+		handler: function( event ) {
+			var currentX, currentY, timeout,
+			args = arguments,
+			target = $( event.target ),
+			previousX = event.pageX,
+			previousY = event.pageY;
+
+			function track( event ) {
+				currentX = event.pageX;
+				currentY = event.pageY;
+			};
+
+			function clear() {
+				target
+				.unbind( "mousemove", track )
+				.unbind( "mouseout", clear );
+				clearTimeout( timeout );
+			}
+
+			function handler() {
+				var prop,
+				orig = event;
+
+				if ( ( Math.abs( previousX - currentX ) +
+					Math.abs( previousY - currentY ) ) < 7 ) {
+					clear();
+
+				event = $.Event( "hoverintent" );
+				for ( prop in orig ) {
+					if ( !( prop in event ) ) {
+						event[ prop ] = orig[ prop ];
+					}
+				}
+				// Prevent accessing the original event since the new event
+				// is fired asynchronously and the old event is no longer
+				// usable (#6028)
+				delete event.originalEvent;
+
+				target.trigger( event );
+				} else {
+					previousX = currentX;
+					previousY = currentY;
+					timeout = setTimeout( handler, 100 );
+				}
+			}
+
+			timeout = setTimeout( handler, 100 );
+			target.bind({
+				mousemove: track,
+				mouseout: clear
+			});
+		}
+	};
+	</script>
+</body>
+</html>
